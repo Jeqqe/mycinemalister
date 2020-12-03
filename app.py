8a71c7ac817050f0d43d3405c7f84162f64a903c
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2020-12-03 12:58:55 +0200

    New links and templates

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_restful import Api
 
 from config import Config
-import resources, extensions
+from resources.page import HomePage, UserHomePage, MovieList, LoginPage, RegisterPage
+import resources
+import extensions
+
 
 def create_app():
 
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/static')
     app.config.from_object(Config)
 
     # Defines all the extensions that the application uses (extensions/__init__.py)
@@ -15,6 +19,29 @@ def create_app():
     resources.init_app(app)
 
     return app
+
+
+def register_resources(app):
+    api = Api(app)
+
+    api.add_resource(HomePage, '/')
+    api.add_resource(MovieList, '/movie-lists/')
+    api.add_resource(UserHomePage, '/users/home')
+
+    # Tämä tulee käyttöön, kun meillä on usersit tehty:
+    # api.add_resource(UserHomePage, '/users/<string:username>/home')
+
+    # Luodaan uusi lista
+    # api.add_resource(#, '/users/<string:username>/create')
+
+    # Editoidaan tietty lista
+    # api.add_resource(#, '/users/<string:username>/<int:list_id>/edit')
+
+    # Poistetaan tietty lista
+    # api.add_resource(#, '/users/<string:username>/<int:list_id>/delete')
+
+    api.add_resource(LoginPage, '/login')
+    api.add_resource(RegisterPage, '/register')
 
 
 if __name__ == "__main__":
--- resources/page.py
+++ resources/page.py
@@ -8,7 +8,27 @@ class HomePage(Resource):
         return make_response(render_template('index.html'), 200, headers)
 
 
+class UserHomePage(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('user_home.html'), 200, headers)
+
+
+class MovieList(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('movie_lists.html'), 200, headers)
+
+
+# Authentication
+
 class LoginPage(Resource):
     def get(self):
         headers = {'Content-Type': 'text/html'}
         return make_response(render_template('login.html'), 200, headers)
+
+
+class RegisterPage(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('register.html'), 200, headers)
